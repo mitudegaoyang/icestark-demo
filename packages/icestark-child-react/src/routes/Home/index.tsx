029ddfcdc50386254b660e89fe448b6bbdfdd2bc
迷途的羔羊 <13683265113@163.com>
2021-02-01 17:52:06 +0800

    feat: 微应用间跳转

--- packages/icestark-child-react/src/routes/Home/index.tsx
+++ packages/icestark-child-react/src/routes/Home/index.tsx
@@ -3,14 +3,14 @@ import PageTitle from '../../components/PageTitle';
 import { Layout, Button } from 'antd';
 import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
+import { appHistory, AppLink } from '@ice/stark-app';
 
 const { Content } = Layout;
 
 export default function List() {
 
   function handleHistoryPush() {
-    console.log('/angular');
-    // appHistory.push('/angular');
+    appHistory.push('/angular');
   };
 
   return (
@@ -20,7 +20,7 @@ export default function List() {
         <Link className={styles.link} to="/list">Link子应用内跳转</Link>
         <br />
         <br />
-        {/* <AppLink className={styles.link} to="/waiter">AppLink子应用间跳转</AppLink> */}
+        <AppLink className={styles.link} to="/seller">AppLink子应用间跳转</AppLink>
         <br />
         <br />
         <Button type="primary" onClick={handleHistoryPush}>appHistory子应用间跳转 </Button>
