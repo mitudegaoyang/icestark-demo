5c1e1f51741e5ac3915875fc86c33ca9fa42162b
迷途的羔羊 <13683265113@163.com>
2021-02-04 14:57:05 +0800

    feat: 基座项目跳转

--- packages/icestark-layout/src/pages/About/index.tsx
+++ packages/icestark-layout/src/pages/About/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'ice';
 
+const baseUrl = localStorage.baseUrl
+
 const About = () => {
   return (
     <div>
       <h1>About page</h1>
-      <div><Link to="/">Home</Link></div>
-      <div><Link to="/seller/list">sellerlist</Link></div>
+      <div><Link to={baseUrl + "/"}>Home</Link></div>
+      <div><Link to={baseUrl + "/seller/list"}>sellerlist</Link></div>
     </div>
   );
 };
--- packages/icestark-layout/src/pages/Home/index.tsx
+++ packages/icestark-layout/src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'ice';
 
+const baseUrl = localStorage.baseUrl
+
 const Home = () => {
   return (
     <div>
       <h1>home page</h1>
-      <div><Link to="/about">About</Link></div>
-      <div><Link to="/seller">seller</Link></div>
+      <div><Link to={baseUrl + "/about"}>About</Link></div>
+      <div><Link to={baseUrl + "/seller"}>seller</Link></div>
     </div>
   );
 };
