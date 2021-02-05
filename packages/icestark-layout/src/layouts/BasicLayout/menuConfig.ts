63c1e337543533b07313dbe49c82f4b038e89b3b
迷途的羔羊 <13683265113@163.com>
2021-02-05 16:03:11 +0800

    fix: 修复首次打开线上获取不到二级域名导致URL异常及刷新404问题

--- packages/icestark-layout/src/layouts/BasicLayout/menuConfig.ts
+++ packages/icestark-layout/src/layouts/BasicLayout/menuConfig.ts
@@ -1,5 +1,5 @@
 const headerMenuConfig = [];
-const baseUrl = localStorage.baseUrl
+const baseUrl = localStorage.baseUrl || (process.env.NODE_ENV === "development" ? "" : "/icestark-demo")
 const asideMenuConfig = [
   {
     name: 'Home',
